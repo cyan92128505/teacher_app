d7dfea9a4b60755a4a7c812f92f8562202bdce1f
cyan92128505 <cyan92128505@gmail.com>
2023-10-10 23:37:41 +0800

    ✨ feat: Add routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -4,14 +4,20 @@ import 'package:teacher/views/splash.dart';
 
 final _parentKey = GlobalKey<NavigatorState>();
 
+const rootPath = '/';
+const loginPath = '/login';
+const homePath = '/home';
+const teacherPath = '/teacher';
+const teacherDetailPath = '/teacher/:id';
+
 final router = GoRouter(
   navigatorKey: _parentKey,
-  initialLocation: '/',
+  initialLocation: rootPath,
   routes: [
     GoRoute(
       parentNavigatorKey: _parentKey,
-      path: '/',
-      name: '/',
+      path: rootPath,
+      name: rootPath,
       pageBuilder: (context, state) => const MaterialPage(child: SplashView()),
     ),
   ],
